eebfa1a6baee0398dca0024be0908e3dbbd91b2f
Ergun Malay <Ergun.Malay.2@city.ac.uk>
2026-05-21 14:29:34 +0100

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> mapS = new HashMap<>();
+        HashMap<Character, Integer> mapT = new HashMap<>();
+
+
+        if(t.length() != s.length()) return false;
+
+        for(char c : s.toCharArray()) {
+            mapS.put(c, mapS.getOrDefault(c, 0) + 1);
+        }
+        for(char c : t.toCharArray()) {
+            mapT.put(c, mapT.getOrDefault(c, 0) + 1);
+        }
+        return mapS.equals(mapT);
+    }
+}
